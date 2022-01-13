2f7972cd2712cf5bc8badfdeb5a804884f7d6e8b
Rhodonite <rhodonite@hotmail.com>
2022-01-13 16:36:25 +0800

    Update Test1.java

--- app/src/main/java/com/rhodonite/fragmenttab/Test1.java
+++ app/src/main/java/com/rhodonite/fragmenttab/Test1.java
@@ -9,19 +9,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Test1 extends Fragment {
-
+    private TextView mTxtR;
+    
     public void onAttach(Activity activity) {
         super.onAttach(activity);
     }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_test1,container, false);
-        return view;
-    }
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
+ 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+	{
+		// Fragment即將在螢幕上顯示時執行此方法
+		return inflater.inflate(R.layout.activity_test1, container, false);
+	}
+ 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) 
+	{
+		// 在這個方法中取得並定義Fragment的介面元件
+		super.onActivityCreated(savedInstanceState);
+		
+		mTxtR = (TextView) getView().findViewById(R.id.textView);
+		//...
+	}
+ 
+	@Override
+	public void onPause() 
+	{
+		// 當Fragment要從螢幕消失時執行此方法
+		super.onPause();
+	}
 
 }
